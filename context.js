cb7436771714b0c01a64863a1745f3dafc630d41
Sam Bisbee <sam@sbisbee.com>
2011-05-04 17:52:32 -0400

    Restructuring localStorage keys, run conditions, and not waiting on XHR to tell us we can mark ourselves as having been here. Reduces multiple runs on domains that don't have couch running.

--- context.js
+++ context.js
@@ -1,10 +1,15 @@
-var url = 'http://'+window.location.hostname+':5984';
-var storageKey = 'futonFinder-'+url;
+var STORAGE_KEYS = {
+  ALREADY_RAN: 'futonFinderWashere',
+  FUTON_URL: 'futonURL'
+};
 
-if(localStorage.getItem(storageKey) !== 'true' && !Boolean(window.location.origin.match(/:5984/)))
-{
-  chrome.extension.sendRequest({ url: url }, function(data) {
-    if(data)
-      localStorage.setItem(storageKey, true);
+if(localStorage.getItem(STORAGE_KEYS.ALREADY_RAN) !== 'true' && !Boolean(window.location.origin.match(/:5984/))) {
+  localStorage.setItem(STORAGE_KEYS.ALREADY_RAN, true);
+
+  var url = 'http://'+window.location.hostname+':5984';
+
+  chrome.extension.sendRequest({ url: url }, function(futonFound) {
+    if(futonFound)
+      localStorage.setItem(STORAGE_KEYS.FUTON_URL, url);
   });
 }
