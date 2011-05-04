be0fd545871ec8c7d524c1bbf1896ed36136f84d
Sam Bisbee <sam@sbisbee.com>
2011-05-04 17:41:00 -0400

    Only notify if we haven't noticed before.

--- context.js
+++ context.js
@@ -1,8 +1,8 @@
-if(!Boolean(window.location.origin.match(/:5984/)))
-{
-  var url = 'http://'+window.location.hostname+':5984';
-  var storageKey = 'futonFinder-'+url;
+var url = 'http://'+window.location.hostname+':5984';
+var storageKey = 'futonFinder-'+url;
 
+if(localStorage.getItem(storageKey) !== 'true' && !Boolean(window.location.origin.match(/:5984/)))
+{
   chrome.extension.sendRequest({ url: url }, function(data) {
     if(data)
       localStorage.setItem(storageKey, true);
